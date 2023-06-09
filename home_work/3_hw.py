ee7ddbd2d6a42819b05a37b1e0ffae01edfc81b5
keterum <keterum@gmail.com>
2023-06-09 19:33:27 +0300

    09.06.2023

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -1,37 +1,46 @@
-a = 7
-b = 7
-if a > b:
-    print(a)
-elif a < b:
-    print(b)
-else:
-    print(a, '=', b)
-
-c = 136
-d = 1
-if c - d == 135 or d - c == 135:
-    print('yes')
-else:
-    print('no')
+def task_2():
+    a = 7
+    b = 7
+    if a > b:
+        print(a)
+    elif a < b:
+        print(b)
+    else:
+        print(a, '=', b)
+task_2()
 
 
-Month = 2
-if Month == 1 or 2 or 12:
-    print('зима')
-elif Month == 3 or 4 or 5:
-    print('весна')
-elif Month == 6 or 7 or 8:
-    print('лето')
-elif Month == 9 or 10 or 11:
-    print('осень')
-else:
-    print('Некорректно')
+def task_3():
+    c = 136
+    d = 1
+    if c - d == 135 or d - c == 135:
+        print('yes')
+    else:
+        print('no')
+task_3()
 
-e = 6
-f = 11
-g = 34
-if e > 10 and f > 10 and g > 10:
-    print('yes')
-else:
-    print('no')
 
+def task_4():
+    Month = 2
+    if Month == 1 or 2 or 12:
+        print('зима')
+    elif Month == 3 or 4 or 5:
+        print('весна')
+    elif Month == 6 or 7 or 8:
+        print('лето')
+    elif Month == 9 or 10 or 11:
+        print('осень')
+    else:
+        print('Некорректно')
+task_4()
+
+
+def task_5():
+    e = 6
+    f = 11
+    g = 34
+    if e > 10 and f > 10 and g > 10:
+        print('yes')
+    else:
+        print('no')
+task_5()
---
+++ python_trening/task_9_oop.py
@@ -0,0 +1,5 @@
+class Button:
+
+    def __init__(self, text, link):
+        self.text = text
+        self.link = link
